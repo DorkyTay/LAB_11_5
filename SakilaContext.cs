5887c2eef9acb4d817c28c3065523694e2035ca7
DorkyTay <67008340+DorkyTay@users.noreply.github.com>
2020-07-31 23:47:39 -0400

    Add project files.

---
+++ SakilaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB_11_5
+{
+    class SakilaContext : DbContext
+    {
+        public DbSet<Film> Film { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost\sqlexpress;Database=sakila;Trusted_Connection=True;");
+        }
+
+    }
+}
